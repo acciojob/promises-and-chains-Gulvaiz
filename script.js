a2b736951776c95e92d7ba3187f39856a3131e15
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-20 15:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const form = document.getElementById("formSubmit")
+
+form.addEventListener("submit", (event) => {
+       event.preventDefault();
+
+       const name = document.getElementById("name").value
+       const age = document.getElementById("age").value
+
+       if(!name || !age){
+            alert("inputs cannot be empty.")
+            return
+       }
+
+       const ageV = Number(age)
+           const ageForm = new Promise((resolve, reject) => {
+                 setInterval((name,age) => {
+                      if(age>18){
+                         resolve (``)
+                      }
+                      else{
+                         reject(``)
+                      }
+                 }, 4000)
+            }).then((message) => alert(message) )
+            .catch((error) => alert(error))
+       
+} )
+
